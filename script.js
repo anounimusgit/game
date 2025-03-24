0cbd3e130ba28e3220476ae5a02344be60c555d3
anounimusgit <defaultgit@gmail.com>
2025-03-24 17:56:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -132,3 +132,14 @@ restartBtn.addEventListener("click", () => {
 document.addEventListener('gesturestart', function (e) {
   e.preventDefault();
 });
+
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', function (event) {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
